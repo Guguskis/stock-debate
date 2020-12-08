fc8913e7294ee46d3295e0b35a4266ffa21db2da
Matas Liutikas <matasliutikas@gmail.com>
2020-12-08 14:52:21 +0200

    updated date range for opinions

--- community-service/src/main/java/lt/liutikas/stockdebate/model/DateRange.java
+++ community-service/src/main/java/lt/liutikas/stockdebate/model/DateRange.java
@@ -5,9 +5,8 @@ import java.time.temporal.ChronoUnit;
 
 public enum DateRange {
     DAY(1, ChronoUnit.DAYS),
-    TWO_DAYS(2, ChronoUnit.DAYS),
-    FIVE_DAYS(5, ChronoUnit.DAYS),
-    TWO_WEEKS(2, ChronoUnit.WEEKS),
+    THREE_DAYS(3, ChronoUnit.DAYS),
+    WEEK(1, ChronoUnit.WEEKS),
     MONTH(1, ChronoUnit.MONTHS),
     THREE_MONTHS(3, ChronoUnit.MONTHS),
     YEAR(1, ChronoUnit.YEARS);
