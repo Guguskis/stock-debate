63b29cd8ab9e0e0c0ccaa4465f2871c414087347
Matas Liutikas <matasliutikas@gmail.com>
2021-01-10 18:22:06 +0200

    readded intendations and improved logging for start-kubernetes-resources.ps1

--- scripts/start-kubernetes-resources.ps1
+++ scripts/start-kubernetes-resources.ps1
@@ -17,31 +17,43 @@ class KubernetesResource {
 }
 
 $kubernetesResources = @(
-[KubernetesResource]::new('authentication-service', 'authentication-service', 'authentication-deployment');
-[KubernetesResource]::new('discussion-service', 'discussion-service', 'discussion-deployment');
-[KubernetesResource]::new('community-service', 'community-service', 'community-deployment');
-[KubernetesResource]::new('stock-service', 'stock-service', 'stock-deployment');
-[KubernetesResource]::new('stock-debate-api', 'stock-debate-api-service', 'stock-debate-api-deployment');
+    [KubernetesResource]::new('authentication-service', 'authentication-service', 'authentication-deployment');
+    [KubernetesResource]::new('discussion-service', 'discussion-service', 'discussion-deployment');
+    [KubernetesResource]::new('community-service', 'community-service', 'community-deployment');
+    [KubernetesResource]::new('stock-service', 'stock-service', 'stock-deployment');
+    [KubernetesResource]::new('stock-debate-api', 'stock-debate-api-service', 'stock-debate-api-deployment');
 )
 
 $ingressName = 'stock-debate-ingress'
 
+Write-Output "----------------building docker images----------------------------------------------"
 & minikube -p minikube docker-env | Invoke-Expression
 $kubernetesResources | ForEach-Object {
 docker build -t $_.module "$rootFolder/$($_.module)"
 }
+Write-Output "----------------docker images built   ----------------------------------------------"
 
 If ($startServices) {
-$kubernetesResources | ForEach-Object {
-kubectl apply -f "$rootFolder/$($_.module)/$($_.service).yml"
-}
+
+    Write-Output "----------------starting kubernetes services------------------------------------"
+    $kubernetesResources | ForEach-Object {
+        kubectl apply -f "$rootFolder/$($_.module)/$($_.service).yml"
+    }
+} else {
+    Write-Output "----------------kubernetes services start was turned off------------------------"
 }
 If ($startDeployments) {
-$kubernetesResources | ForEach-Object {
-kubectl apply -f "$rootFolder/$($_.deployment).yml"
-}
+    Write-Output "----------------starting kubernetes deployments---------------------------------"
+    $kubernetesResources | ForEach-Object {
+        kubectl apply -f "$rootFolder/$($_.deployment).yml"
+    }
+} else {
+    Write-Output "----------------kubernetes deployments start was turned off---------------------"
 }
 
 If ($startIngress) {
-kubectl apply -f "$rootFolder/$ingressName.yml"
+    Write-Output "----------------starting kubernetes ingress-------------------------------------"
+    kubectl apply -f "$rootFolder/$ingressName.yml"
+} else {
+    Write-Output "----------------kubernetes ingress start was turned off-------------------------"
 }
